78aa602e5950880e43741d4662314c977cbbcde5
Abwuds <darealbaxter@gmail.com>
2015-12-03 21:02:36 +0100

    [UPD] Rename ServerApplication name.

--- Server/src/fr/upem/rmirest/bilmancamp/ServerApplication.java
+++ Server/src/fr/upem/rmirest/bilmancamp/ServerApplication.java
@@ -1,5 +1,6 @@
+package fr.upem.rmirest.bilmancamp;
 
-public class RandomClass {
+public class ServerApplication {
 	public static void main(String[] args) {
 		System.out.println("Can I push ??");
 	}
