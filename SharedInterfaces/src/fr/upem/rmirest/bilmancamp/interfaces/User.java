4e55d1b383006c656bc8214bce48ec4c50a31704
qcampos <quentin.campos@gmail.com>
2015-12-04 14:34:16 +0100

    [ADD] Basic version of the User shared interface.
    This first version of User is probably not the final one but is needed
    for further implementation.

--- SharedInterfaces/src/fr/upem/rmirest/bilmancamp/interfaces/User.java
+++ SharedInterfaces/src/fr/upem/rmirest/bilmancamp/interfaces/User.java
@@ -3,10 +3,41 @@ package fr.upem.rmirest.bilmancamp.interfaces;
 public interface User {
 
 	/**
+	 * @return the status of the user.
+	 */
+	public String getStatus();
+
+	/**
+	 * @return the user's first name.
+	 */
+	public String getFirstName();
+
+	/**
+	 * @return the user's last name.
+	 */
+	public String getLastName();
+
+	/**
+	 * @return the user's library card number.
+	 */
+	public int getCardNumber();
+
+	/**
+	 * Indicates if the connection data are valid for this user.
+	 * 
+	 * @param id
+	 *            the id of the user which is firstName + lastName + cardNumber
+	 *            concatenate.
+	 * @param password
+	 *            the encrypted password.
+	 * @return true if the given connection data are valid for this user.
+	 */
+	public boolean isLoginValid(String id, String password);
+
+	/**
 	 * Disconnect the used. Standard values are still accessible, but not
 	 * interactions with the library.
 	 */
 	public void disconnect();
 
-	// TODO getters.
 }
