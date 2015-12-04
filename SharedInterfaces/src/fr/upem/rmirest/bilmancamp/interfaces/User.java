e145d56ce0812bf2ffe8a5c6d4b12ee276c46bb3
qcampos <quentin.campos@gmail.com>
2015-12-04 14:42:47 +0100

    [UPD] Correction of the documentation in the User interface.

--- SharedInterfaces/src/fr/upem/rmirest/bilmancamp/interfaces/User.java
+++ SharedInterfaces/src/fr/upem/rmirest/bilmancamp/interfaces/User.java
@@ -3,40 +3,39 @@ package fr.upem.rmirest.bilmancamp.interfaces;
 public interface User {
 
 	/**
-	 * @return the status of the user.
+	 * @return the status of the current user.
 	 */
 	public String getStatus();
 
 	/**
-	 * @return the user's first name.
+	 * @return the current user's first name.
 	 */
 	public String getFirstName();
 
 	/**
-	 * @return the user's last name.
+	 * @return the current user's last name.
 	 */
 	public String getLastName();
 
 	/**
-	 * @return the user's library card number.
+	 * @return the current user's library card number.
 	 */
 	public int getCardNumber();
 
 	/**
-	 * Indicates if the connection data are valid for this user.
+	 * Indicates if the given credentials are valid for the current user.
 	 * 
 	 * @param id
 	 *            the id of the user which is firstName + lastName + cardNumber
 	 *            concatenate.
 	 * @param password
 	 *            the encrypted password.
-	 * @return true if the given connection data are valid for this user.
+	 * @return true if the given connection data are valid for the current user.
 	 */
 	public boolean isLoginValid(String id, String password);
 
 	/**
-	 * Disconnect the user. Standard values are still accessible, but not
-	 * interactions with the library.
+	 * Disconnect the current user.
 	 */
 	public void disconnect();
 
