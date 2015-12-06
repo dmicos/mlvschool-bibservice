fd86188dc131dd37c601c448085a1a4a255f7948
Abwuds <darealbaxter@gmail.com>
2015-12-06 23:43:45 +0100

    [ADD] Begin of various css & fxml rules of implementation.

--- Client-MLVSchool/src/application/Main.java
+++ Client-MLVSchool/src/application/Main.java
@@ -1,25 +1,36 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root, 500, 400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Pane screen = loadScreen("../scenes/connexionScreen/connexionScreen.fxml");
+			Scene scene = new Scene(screen, 1200, 800);
+			loadCSS(scene, "../css/florange.css");
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
+		} catch (IOException e) {
+			System.err.println("Cannot load the scene : " + e.getCause());
+			return;
 		}
 	}
-	
+
+	private void loadCSS(Scene scene, String cssPath) {
+		scene.getStylesheets().add(getClass().getResource(cssPath).toExternalForm());
+	}
+
+	private Pane loadScreen(String screenPath) throws IOException {
+		return FXMLLoader.load(getClass().getResource(screenPath));
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
