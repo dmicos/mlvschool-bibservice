e6a9ca53f24e2e052ad82d4ae98dc5853f1f5fab
Abwuds <darealbaxter@gmail.com>
2015-12-11 12:53:48 +0100

    [ADD] Stuff which prevent me to push.

--- Server/src/fr/upem/rmirest/bilmancamp/models/UserImpl.java
+++ Server/src/fr/upem/rmirest/bilmancamp/models/UserImpl.java
@@ -77,7 +77,7 @@ public class UserImpl extends UnicastRemoteObject implements User {
 
 		@Override
 		public String toString() {
-			return String.format("%s n°%s - %s %s", getStatus(), getCardNumber(), getFirstName(), getLastName());
+			return String.format("%s nï¿½%s - %s %s", getStatus(), getCardNumber(), getFirstName(), getLastName());
 		}
 
 		@Override
